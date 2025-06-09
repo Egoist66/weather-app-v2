ac4ea863fc8228735da937df5a1b136e7105f44a
Farid Mahmudov <razormad666@gmail.com>
2025-06-09 16:25:00 +0300

    update-1

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,21 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="app-layout">
+
+        <header v-if="$slots.header">
+            <slot name="header" />
+        </header>
+
+        <main>
+            <slot name="default" />
+        </main>
+
+    </div>
+</template>
+<style scoped>
+
+</style>
