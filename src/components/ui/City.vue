dfa6425e00a334793fdf9d4485c1adc9673ddda2
Farid Mahmudov <razormad666@gmail.com>
2025-06-14 13:47:30 +0300

    update-6

--- src/components/ui/City.vue
+++ src/components/ui/City.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useWeather } from "../../composables/useWeather";
-import { timeFormatter } from '../../utils/time-formatter';
-
+import { timeFormatter } from "../../utils/time-formatter";
 
 const { weatherStore, route } = await useWeather();
 
@@ -10,21 +9,20 @@ const isPreviewOn = route.query.preview === "true";
 
 <template>
   <div class="flex flex-col flex-1 items-center">
-
     <!-- Banner -->
     <div
       v-if="isPreviewOn"
       class="text-white p-4 bg-weather-secondary rounded-md shadow-md w-full text-center"
     >
       <p>
-        You are currently previewing this city, click on <b>"+"</b> icon to start tracking the city
+        You are currently previewing this city, click on <b>"+"</b> icon to start tracking
+        the city
       </p>
-
     </div>
 
     <!-- Weather Overview -->
 
-    <div class="flex-col flex items-center gap-8 justify-center text-white py-12">
+    <div class="flex-col flex items-center gap-7 justify-center text-white py-12">
       <h1 class="text-8xl font-bold">
         {{ route.params.city }}
       </h1>
@@ -35,14 +33,22 @@ const isPreviewOn = route.query.preview === "true";
         {{ Math.round(weatherStore.weatherData?.current.temp!) || 'N/A' }}&deg;
       </p>
       <p class="text-xl">
-        {{ weatherStore.weatherData?.current.weather[0].description.toUpperCase() || 'N/A' }}
+        {{
+          weatherStore.weatherData?.current.weather[0].description.toUpperCase() || "N/A"
+        }}
       </p>
+      <div>
+        <img
+          class="w-[150px] h-auto"
+          :src="`http://openweathermap.org/img/wn/${weatherStore.weatherData?.current.weather[0].icon}@2x.png`"
+          :alt="weatherStore.weatherData?.current.weather[0].description"
+        />
+      </div>
       <p>
-        {{ route.query.lang === 'ru' ? 'Ощущается как' : 'Feels like' }} - {{ Math.round(weatherStore.weatherData?.current.feels_like!) || 'N/A' }}&deg;
+        {{ route.query.lang === "ru" ? "Ощущается как" : "Feels like" }} -
+        {{ Math.round(weatherStore.weatherData?.current.feels_like!) || 'N/A' }}&deg;
       </p>
     </div>
-
-    
   </div>
 </template>
 
